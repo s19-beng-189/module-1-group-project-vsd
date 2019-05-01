4b88731a4d897cc75cbf129837a536dc0cddee1f
Yijie He <yih148@ucsd.edu>
2019-05-01 12:59:19 -0700

    Create CV_now2.m

---
+++ CV_now2.m
@@ -0,0 +1,18 @@
+%file used in alternating the heartbeats 
+function CV=CV_now2(t,CVS,CVD)
+%filename: CV_now2.m   
+
+T2 =0.02;  %alternate the heartbeats 
+TS2=0.008; 
+tauS2 = 0.004;
+tauD2 = 0.012;
+
+tc=rem(t,T2); %tc=time in the current cycle, 
+             %measured from start of systole.
+if(tc<TS2)
+  e=(1-exp(-tc/tauS2))/(1-exp(-TS2/tauS2));
+  CV=CVD*(CVS/CVD)^e;
+else
+  e=(1-exp(-(tc-TS2)/tauD2))/(1-exp(-(T2-TS2)/tauD2));
+  CV=CVS*(CVD/CVS)^e;
+end
